cf36b6afae3bb3072384f314fceb96996ac69b1d
judgejudes <judylee2021@u.northwestern.edu>
2020-09-23 22:30:14 -0400

    added Banner

--- App.js
+++ App.js
@@ -2,7 +2,29 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 const schedule = {
-  title: "CS Courses for 2018-2019"
+  "title": "CS Courses for 2018-2019",
+  "courses": [
+    {
+      "id": "F101",
+      "title": "Computer Science: Concepts, Philosophy, and Connections",
+      "meets": "MWF 11:00-11:50"
+    },
+    {
+      "id": "F110",
+      "title": "Intro Programming for non-majors",
+      "meets": "MWF 10:00-10:50"
+    },
+    {
+      "id": "F111",
+      "title": "Fundamentals of Computer Programming I",
+      "meets": "MWF 13:00-13:50"
+    },
+    {
+      "id": "F211",
+      "title": "Fundamentals of Computer Programming II",
+      "meets": "TuTh 12:30-13:50"
+    }
+  ]
 };
 
 // App is a component (function that when called returns piece of UI,
@@ -10,10 +32,15 @@ const schedule = {
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.bannerStyle}>{schedule.title}</Text>
+      {/* <Text style={styles.bannerStyle}>{schedule.title}</Text> */}
+      <Banner title={schedule.title} />
     </View>
   );
 }
+
+const Banner = ({title}) => ( //title is part of parameter list
+  <Text style={styles.bannerStyle}>{title}</Text>
+)
 
 const styles = StyleSheet.create({
   container: {
