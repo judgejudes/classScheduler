e3dfadb57c01eb5ccaaa97db643b4f5e03c38a59
judgejudes <judylee2021@u.northwestern.edu>
2020-09-27 08:35:51 -0400

    fetches data from url

--- App.js
+++ App.js
@@ -1,41 +1,59 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { SafeAreaView, StyleSheet, Text } from 'react-native';
 import CourseList from './components/CourseList';
 
-const schedule = {
-  "title": "CS Courses for 2018-2019",
-  "courses": [
-    {
-      "id": "F101",
-      "title": "Computer Science: Concepts, Philosophy, and Connections",
-      "meets": "MWF 11:00-11:50"
-    },
-    {
-      "id": "F110",
-      "title": "Intro Programming for non-majors",
-      "meets": "MWF 10:00-10:50"
-    },
-    {
-      "id": "F111",
-      "title": "Fundamentals of Computer Programming I",
-      "meets": "MWF 13:00-13:50"
-    },
-    {
-      "id": "F211",
-      "title": "Fundamentals of Computer Programming II",
-      "meets": "TuTh 12:30-13:50"
-    }
-  ]
-};
+// const schedule = {
+//   "title": "CS Courses for 2018-2019",
+//   "courses": [
+//     {
+//       "id": "F101",
+//       "title": "Computer Science: Concepts, Philosophy, and Connections",
+//       "meets": "MWF 11:00-11:50"
+//     },
+//     {
+//       "id": "F110",
+//       "title": "Intro Programming for non-majors",
+//       "meets": "MWF 10:00-10:50"
+//     },
+//     {
+//       "id": "F111",
+//       "title": "Fundamentals of Computer Programming I",
+//       "meets": "MWF 13:00-13:50"
+//     },
+//     {
+//       "id": "F211",
+//       "title": "Fundamentals of Computer Programming II",
+//       "meets": "TuTh 12:30-13:50"
+//     }
+//   ]
+// };
 
-const Banner = ({title}) => ( //title is part of parameter list
-  <Text style={styles.bannerStyle}>{title}</Text>
+const Banner = ({ title }) => ( //title is part of parameter list
+  <Text style={styles.bannerStyle}>{title || '[loading...]'}</Text>
 );
+
+
 
 
 // App is a component (function that when called returns piece of UI,
 //which is the View component to hold UI elements and Text component to hold text
 const App = () => {
+  //initial course schedule object; initial value is object with empty title and empty list of courses
+  const [schedule, setSchedule] = useState({ title: '', courses: [] });
+
+  const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
+
+  //get schedule JSON data, and store using setSchedule() created by useState()
+  useEffect(() => {
+    const fetchSchedule = async () => {
+      const response = await fetch(url);
+      if (!response.ok) throw response;
+      const json = await response.json();
+      setSchedule(json);
+    }
+    fetchSchedule();
+  }, [])
+
   return (
     <SafeAreaView style={styles.container}>
       <Banner title={schedule.title} />
