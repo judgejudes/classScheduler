a890531e03f6436e13a52e43b64cc7e9fbe20f0a
judgejudes <judylee2021@u.northwestern.edu>
2020-09-28 23:42:43 -0400

    filtering

--- components/CourseList.js
+++ components/CourseList.js
@@ -1,15 +1,32 @@
-import React from 'react';
-import { StyleSheet, ScrollView, View} from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, ScrollView, View } from 'react-native';
 import Course from './Course';
+import TermSelector from './TermSelector';
 
-const CourseList = ({ courses }) => (
+//creating object to map letters to terms, then making list of term names to make buttons with
+const termMap = { F: 'Fall', W: 'Winter', S: 'Spring' };
+const terms = Object.values(termMap);
+
+const getCourseTerm = course => (
+  termMap[course.id.charAt(0)]
+);
+
+const CourseList = ({ courses }) => {
+  const [selectedTerm, setSelectedTerm] = useState('Fall');
+
+  //list of courses in selected term
+  const termCourses = courses.filter(course => selectedTerm === getCourseTerm(course));
+
+  return (
     <ScrollView>
+      {/* passing selected term as a property to term selector */}
+      <TermSelector selectedTerm = {selectedTerm} setSelectedTerm={setSelectedTerm}/>
       <View style={styles.courseList}>
-      {/* list of course components */}
-        { courses.map(course => <Course key={course.id} course={ course } />)}
-      </View>
+        { termCourses.map(course => <Course key={course.id} course={course} />) }
+        </View>
     </ScrollView>
   );
+};
 
 const styles = StyleSheet.create({
   courseList: {
